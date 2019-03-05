3a4b96efac0a55e1bd647e8ac949f027f292e5c4
zf <413273029@qq.com>
2019-03-05 20:01:55 +0800

    MVC 思想编程及添加评论区功能实现

---
+++ sticky-topBar.js
@@ -0,0 +1,28 @@
+!function () {
+  var view = document.querySelector('#topNavBar')
+
+  var controller = {
+    view:null,
+    init: function (view) {
+      this.view = view
+      this.bindEvents()
+    },
+    bindEvents: function () {
+      // var view = this.view
+      window.addEventListener('scroll', (x)=>{  //箭头函数里面没有this的相关指定，里面的this 不变
+        if (window.scrollY > 5) {
+          this.active()
+        } else {
+          this.deactive()
+        }
+      })
+    },
+    active:function () {
+      this.view.classList.add('sticky')
+    },
+    deactive:function () {
+      this.view.classList.remove('sticky')
+    }
+  }
+  controller.init(view)
+}.call()
